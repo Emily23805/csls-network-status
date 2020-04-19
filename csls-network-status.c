0c84b3200e36786d59ced99689d0b7d2ccd556d0
kudansul <33092539+Emily23805@users.noreply.github.com>
2020-04-20 00:59:56 +0900

    Update csls-network-status.c

--- csls-network-status.c
+++ csls-network-status.c
@@ -9,7 +9,7 @@
 
 char t1[30];
 
-/* ltime(); 함수*/
+/* time funtion*/
 void ltime()
 {
 struct tm *date;
@@ -27,6 +27,7 @@ void ltimes()
 
            sprintf( t1, "%d/%d/%d %d:%d:%d\n" , date->tm_year + 1900 , date->tm_mon + 1 , date->tm_mday , date->tm_hour , date->tm_min , date->tm_sec );
 }
+/* end time funtion*/
 
 /* nic-core */
 void nic()
@@ -51,7 +52,7 @@ void nic()
 /* ip-addr*/
 void ipaddr()
 {
-	    struct ifaddrs *ifap, *ifa;
+    struct ifaddrs *ifap, *ifa;
     struct sockaddr_in *sa;
     char *addr;
 
@@ -67,6 +68,32 @@ void ipaddr()
     freeifaddrs(ifap);
 }
 /*end ip-addr*/
+
+/* oneip*/
+void oneip()
+{
+    struct ifaddrs *ifap, *ifa;
+    struct sockaddr_in *sa;
+    char *addr;
+
+    getifaddrs (&ifap);
+    for (ifa = ifap; ifa; ifa = ifa->ifa_next) {
+        if (ifa->ifa_addr && ifa->ifa_addr->sa_family==AF_INET) {
+            sa = (struct sockaddr_in *) ifa->ifa_addr;
+            addr = inet_ntoa(sa->sin_addr);
+                if(strcmp(addr, "127.0.0.1") != 0)
+                {
+                    printf("%s\n",addr);
+                    break;
+                }
+
+        }
+    }
+
+    freeifaddrs(ifap);
+}
+/*end oneip*/
+
 /*boot script codes*/
 void boot()
 {
@@ -88,12 +115,14 @@ void boot()
 	printf("boot time:%s\n",t1);
 }
 /*end boot script codes*/
+
 /* help() */
 void help()
 {
 	printf("enet is clear sky linux series network test program\n========================================================\nuseing enet -tni\nuseing enet -t \n========================================================\n -t [print time]\n -n [print NIC(Network Interface controller , LAN card)]\n -i [print ip address]\n -l [print non lo NIC(Network Interface controller , LAN card)]\n -h [print enet help]\n -r [reboot system]\n========================================================\nReference list\n nic-core() [https://m.blog.naver.com/cksdn788/220379995787]\n ltime(),ltimes() [https://araikuma.tistory.com/597]\n ip-addr(),boot() [https://stackoverflow.com/questions/4139405/how-can-i-get-to-know-the-ip-address-for-interfaces-in-c]\n nicnonlo() [https://pencil1031.tistory.com/66]\n========================================================\nlicences\n Copyright kudansul\n Development by kudansul\n========================================================\nsource code/github\n [https://github.com/fuyubi-network/csls-network-status.git]\n========================================================\n");
 }
 /* end help() */
+
 /* nic-core non-lo ver */
 void nicnonlo()
 {
@@ -117,6 +146,7 @@ void nicnonlo()
     freeifaddrs(addrs);
 }
 /* end nic-core non-lo ver*/
+
 /* reboot this server/workstation/machen*/
 void reboot()
 {
@@ -129,28 +159,30 @@ int main( int argc, char **argv)
    int   param_opt;
 
    opterr   = 0;
-   while( -1 !=( param_opt = getopt( argc, argv, "f:nithrlb")))
+   while( -1 !=( param_opt = getopt( argc, argv, "f:nithrolb")))
    {
       switch( param_opt)
       {
-      case  'f'   :  printf( "%s \n", optarg);
-                     break;
-      case  'n'   :  nic();
+ 	  case  'f'   :  printf( "%s \n", optarg);
+          			         break;
+	  case  'n'   :  nic();
 			  	 	 break;
-	  case	'i'	  :  ipaddr();
+	  case	'i'   :  ipaddr();
 			  		 break;
-      case  't'   :  ltime();
-                     break;
+	  case  't'   :  ltime();
+                     			 break;
 	  case  'h'   :  help();
 			  		 break;
 	  case  'b'   :  boot();
+			  		 break;
+	  case  'o'   :  oneip();
 			  		 break;
 	  case  'l'   :  nicnonlo();
 			  		 break;
 	  case  'r'   :  reboot();
 			  		 break;
-      case  '?'   :  printf( "Try 'enet -h' for more information.\n", optopt);
-                     break;
+	  case  '?'   :  printf( "Try 'enet -h' for more information.\n", optopt);
+          			         break;
       }
    }
    return 0;
